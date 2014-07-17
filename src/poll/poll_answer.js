7232a73ee9517026d8e8f281102795cd5173409e
Anthony Chu <anthony@anthonychu.ca>
2014-07-16 20:03:13 -0700

    Add answer directive (for real this time)

---
+++ src/poll/poll_answer.js
@@ -0,0 +1,21 @@
+﻿(function (module) {
+
+  module.directive('pollAnswer', function ($rootScope) {
+    return {
+      restrict: 'A',
+      requires: '^poll',
+      transclude: true,
+      scope: {
+        answerId: '=answer'
+      },
+      template: '<div><input type="checkbox"><span ng-transclude></span></div>',
+      link: function ($scope, $element, $attrs, ctrl) {
+        $element.on('click', function () {
+          ctrl.pickAnswer($scope.answerId);
+          $rootScope.$emit('pickedAnswer', $scope.answerId);
+        });
+      }
+    }
+  });
+
+})(angular.module('pollAnswer', ['ng', 'pollCtrl']));
