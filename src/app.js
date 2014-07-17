6ef14a404ddff9a30ade4b649894b2efaba61394
Anthony Chu <anthony@anthonychu.ca>
2014-07-16 19:40:53 -0700

    Add poll answer directive

--- src/app.js
+++ src/app.js
@@ -8,7 +8,8 @@ angular.module('TestApp', [
     'phoneCtrl',
     'phoneFilter',
     'pollCtrl',
-    'pollDirective'
+    'pollDirective',
+    'pollAnswer'
 ])
 .config(function($routeProvider) {
     $routeProvider.otherwise({
